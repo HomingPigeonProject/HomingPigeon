0d4a1faa2dab33ab487bbb0c2962c47ee59e15ad
Pierre Thévenet <thevenet.pierre@gmail.com>
2016-10-28 15:52:12 +0900

    logintest

--- LoginTest/login.php
+++ LoginTest/login.php
@@ -1,138 +1,77 @@
 <?php
- ob_start();
- session_start();
- require_once 'dbconnect.php';
+   include("config.php");
+   session_start();
 
- // it will never let you open index(login) page if session is set
- if ( isset($_SESSION['user'])!="" ) {
-  header("Location: home.php");
-  exit;
- }
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form
 
- $error = false;
+      $myusername = mysqli_real_escape_string($db,$_POST['username']);
+      $mypassword = mysqli_real_escape_string($db,$_POST['password']);
 
- if( isset($_POST['btn-login']) ) {
+      $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
+      $result = mysqli_query($db,$sql);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      $active = $row['active'];
 
-  // prevent sql injections/ clear user invalid inputs
-  $email = trim($_POST['email']);
-  $email = strip_tags($email);
-  $email = htmlspecialchars($email);
+      $count = mysqli_num_rows($result);
 
-  $pass = trim($_POST['pass']);
-  $pass = strip_tags($pass);
-  $pass = htmlspecialchars($pass);
-  // prevent sql injections / clear user invalid inputs
+      // If result matched $myusername and $mypassword, table row must be 1 row
 
-  if(empty($email)){
-   $error = true;
-   $emailError = "Please enter your email address.";
-  } else if ( !filter_var($email,FILTER_VALIDATE_EMAIL) ) {
-   $error = true;
-   $emailError = "Please enter valid email address.";
-  }
+      if($count == 1) {
+         session_register("myusername");
+         $_SESSION['login_user'] = $myusername;
 
-  if(empty($pass)){
-   $error = true;
-   $passError = "Please enter your password.";
-  }
-
-  // if there's no error, continue to login
-  if (!$error) {
-
-   $password = hash('sha256', $pass); // password hashing using SHA256
-
-   $res=mysql_query("SELECT userId, userName, userPass FROM users WHERE userEmail='$email'");
-   $row=mysql_fetch_array($res);
-   $count = mysql_num_rows($res); // if uname/pass correct it returns must be 1 row
-
-   if( $count == 1 && $row['userPass']==$password ) {
-    $_SESSION['user'] = $row['userId'];
-    header("Location: home.php");
-   } else {
-    $errMSG = "Incorrect Credentials, Try again...";
+         header("location: welcome.php");
+      }else {
+         $error = "Your Login Name or Password is invalid";
+      }
    }
-
-  }
-
- }
 ?>
-<!DOCTYPE html>
 <html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Coding Cage - Login & Registration System</title>
-<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css"  />
-<link rel="stylesheet" href="style.css" type="text/css" />
-</head>
-<body>
 
-<div class="container">
+   <head>
+      <title>Login Page</title>
 
- <div id="login-form">
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" autocomplete="off">
+      <style type = "text/css">
+         body {
+            font-family:Arial, Helvetica, sans-serif;
+            font-size:14px;
+         }
 
-     <div class="col-md-12">
+         label {
+            font-weight:bold;
+            width:100px;
+            font-size:14px;
+         }
 
-         <div class="form-group">
-             <h2 class="">Sign In.</h2>
+         .box {
+            border:#666666 solid 1px;
+         }
+      </style>
+
+   </head>
+
+   <body bgcolor = "#FFFFFF">
+
+      <div align = "center">
+         <div style = "width:300px; border: solid 1px #333333; " align = "left">
+            <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
+
+            <div style = "margin:30px">
+
+               <form action = "" method = "post">
+                  <label>UserName  :</label><input type = "text" name = "username" class = "box"/><br /><br />
+                  <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
+                  <input type = "submit" value = " Submit "/><br />
+               </form>
+
+               <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
+
             </div>
 
-         <div class="form-group">
-             <hr />
-            </div>
+         </div>
 
-            <?php
-   if ( isset($errMSG) ) {
+      </div>
 
-    ?>
-    <div class="form-group">
-             <div class="alert alert-danger">
-    <span class="glyphicon glyphicon-info-sign"></span> <?php echo $errMSG; ?>
-                </div>
-             </div>
-                <?php
-   }
-   ?>
-
-            <div class="form-group">
-             <div class="input-group">
-                <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span></span>
-             <input type="email" name="email" class="form-control" placeholder="Your Email" value="<?php echo $email; ?>" maxlength="40" />
-                </div>
-                <span class="text-danger"><?php echo $emailError; ?></span>
-            </div>
-
-            <div class="form-group">
-             <div class="input-group">
-                <span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
-             <input type="password" name="pass" class="form-control" placeholder="Your Password" maxlength="15" />
-                </div>
-                <span class="text-danger"><?php echo $passError; ?></span>
-            </div>
-
-            <div class="form-group">
-             <hr />
-            </div>
-
-            <div class="form-group">
-             <button type="submit" class="btn btn-block btn-primary" name="btn-login">Sign In</button>
-            </div>
-
-            <div class="form-group">
-             <hr />
-            </div>
-
-            <div class="form-group">
-             <a href="register.php">Sign Up Here...</a>
-            </div>
-
-        </div>
-
-    </form>
-    </div>
-
-</div>
-
-</body>
+   </body>
 </html>
-<?php ob_end_flush(); ?>
--- LoginTest/logout.php
+++ LoginTest/logout.php
@@ -1,15 +1,7 @@
 <?php
- session_start();
- if (!isset($_SESSION['user'])) {
-  header("Location: index.php");
- } else if(isset($_SESSION['user'])!="") {
-  header("Location: home.php");
- }
+   session_start();
 
- if (isset($_GET['logout'])) {
-  unset($_SESSION['user']);
-  session_unset();
-  session_destroy();
-  header("Location: index.php");
-  exit;
- }
+   if(session_destroy()) {
+      header("Location: login.php");
+   }
+?>
