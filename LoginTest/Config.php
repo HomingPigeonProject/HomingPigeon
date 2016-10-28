00c56c4058134cd65f1aaee7a6b7a2daa7cbfb58
Pierre Thévenet <thevenet.pierre@gmail.com>
2016-10-28 15:44:07 +0900

    logintest

--- LoginTest/Config.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', 'localhost:3036');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', 'team3');
-   define('DB_DATABASE', 'HomingPigeon');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
